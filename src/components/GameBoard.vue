debe2a939e29c98ac65bccc62b9adc6fc6adf66e
courtney l <clandau@learnics.com>
2021-06-09 13:58:05 -0400

    gameboard renders current state of board

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,32 +1,35 @@
 <template>
-  <div class="board-container">
+  <div class="board-container justify-center">
     <div class="board">
-      <div class="cell">X</div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell">O</div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
+      <div class="cell"><p>{{ boardState[0][0] }}</p></div>
+      <div class="cell"><p>{{ boardState[0][1] }}</p></div>
+      <div class="cell"><p>{{ boardState[0][2] }}</p></div>
+      <div class="cell"><p>{{ boardState[1][0] }}</p></div>
+      <div class="cell"><p>{{ boardState[1][1] }}</p></div>
+      <div class="cell"><p>{{ boardState[1][2] }}</p></div>
+      <div class="cell"><p>{{ boardState[2][0] }}</p></div>
+      <div class="cell"><p>{{ boardState[2][1] }}</p></div>
+      <div class="cell"><p>{{ boardState[2][2] }}</p></div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: "GameBoard",
+  props: {
+    boardState: {
+      type: Array,
+      required: true,
+    }
+  }
 };
 </script>
 
 <style scoped>
-.board-container {
-}
 .board {
   width: 500px;
   height: 500px;
-  margin: 0 auto;
   display: grid;
   grid-auto-columns: 1fr;
   grid-auto-rows: 1fr;
--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -1,16 +1,32 @@
 <template>
-  <div></div>
+  <v-container class="pa-10 ma-3">
+    <v-row>
+      <v-col cols="1"></v-col>
+      <v-col cols="10">
+        <GameBoard v-if="state" :boardState="state.board"  />
+      </v-col>
+      <v-col cols="1"></v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import GameBoard from "@/components/GameBoard"
 const io = require("socket.io-client");
 
 export default {
   name: "GamePage",
+  components: { GameBoard },
   props: {
     twoPlayer: {
       type: Boolean,
     },
+  },
+  data() {
+    return {
+      // state: { board: [["X", "O", "X"],["X", "O", "X"], ["X", "O", "X"]]},
+      state: null,
+    }
   },
   created() {
     const socket = io("http://localhost:3000");
@@ -24,6 +40,7 @@ export default {
   methods: {
     handleState(state) {
       console.log(state);
+      this.state = state;
     },
   },
 };
