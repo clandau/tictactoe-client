371c247844050d27c7831a5196e7eb005560c43b
courtney l <clandau@learnics.com>
2021-06-09 20:10:17 -0400

    got tic tac toe board looking good

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -2,9 +2,9 @@
   <v-container>
     <v-layout wrap>
       <v-flex xs12 class="mb-5">
-        <v-container grid-list-xs>
+        <v-container grid-list-xs class="board">
           <v-layout v-for="(row, y) of boardState" :key="y">
-            <div v-for="(val, x) of boardState[y]" :key="y + x" @mouseup="handleChoice(y, x, val)" class="cell">
+            <div v-for="(val, x) of boardState[y]" :key="y + x" @mouseup="handleChoice(y, x, val)" class="cell" :class="'row-' + y">
               <p>{{ val }}</p>
             </div>
           </v-layout>
@@ -35,17 +35,30 @@ export default {
 </script>
 
 <style scoped>
-.board .cell:last-child {
-  grid-area: 3/3;
-}
 .cell {
-  outline: 4px solid #000;
+  /* outline: 4px solid #000; */
   display: flex;
   font-size: 6em;
   font-weight: bold;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: stretch;
   width: 150px;
   height: 150px;
 }
+
+.cell:nth-of-type(1) {
+  border-right: 6px solid black;
+}
+.cell:nth-of-type(3) {
+  border-left: 6px solid black;
+}
+
+.row-0 {
+  border-bottom: 6px solid black;
+}
+.row-1 {
+  border-bottom: 6px solid black;
+}
+
+
 </style>
