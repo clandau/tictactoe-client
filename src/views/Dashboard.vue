845efed3aea4dff1bc227cd0c49c16731d052960
courtney l <clandau@learnics.com>
2021-06-11 18:32:44 -0400

    change sizing of dashboard sidebar

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid class="dashboard-container mx-auto">
     <v-row>
-      <v-col xs="12" md="7">
+      <v-col xs="12" md="8">
         <div class="game-button-container mb-6 pa-5">
           <h1 class="text-h2">Start a new game</h1>
           <div class="button-container pa-6">
@@ -16,7 +16,7 @@
           </div>
         </div>
       </v-col>
-      <v-col xs="12" md="5" class="side-container">
+      <v-col xs="12" md="4" class="side-container">
         <div class="my-2"><LeaderBoard /></div>
         <div class="my-2"><GameHistory /></div
       ></v-col>
